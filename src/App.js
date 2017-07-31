ea2b15dc90bcde457402e3449a6614280fc52903
Siddharth Chauhan <csiddha@elemental.com>
2017-07-30 17:37:24 -0700

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import * as BooksAPI from './BooksAPI.js';
 import './App.css';
 
+import logo from './logo.svg';
+
+import BookShelf from './components/BookShelf.js'
+import SearchBook from './components/SearchBook.js'
+
 class App extends Component {
+
+  state = {
+	books: []
+  }
+
   render() {
     return (
-      <div>
-	  Hello world
-      </div>
+		<Router>
+		  <div>
+		   <Route exact path='/' render={() => (
+			<BookShelf />
+			)}/>
+		   <Route path='/search' component={SearchBook}/>
+          </div>
+		</Router>
     );
   }
 }
---
+++ src/components/SearchBook.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SearchBook extends Component {
+  render() {
+	console.log("Kaush");
+    return (
+      <div>Search Book!</div>
+    )
+  }
+}
+
+export default SearchBook
