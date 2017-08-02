811b5d0e56281a9ea4466ec35c123cb9be3d9d3b
Siddharth Chauhan <csiddha@elemental.com>
2017-08-01 23:42:50 -0700

    Add searchable books

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp'
 
 class SearchBook extends Component {
   state = {
-	  query: ""
+	  query: "",
   }
 
   updateQuery(value){
 	this.setState({ query: value.trim()});
   }
 
+  filterBooks(books, query){
+	var re = new RegExp(query, 'i');
+	var matchingBooks = books.filter(function(el) {
+	  return el.title.match(re);
+	});
+	return matchingBooks;
+  }
+
   render() {
 	const { books } = this.props;
+    var filtered_books = this.filterBooks(books, this.state.query);
+    const listMap = filtered_books.map((book, index) => {
+					   return <li key={book.id}>
+					     <ul>{book.title}</ul>
+					   </li>
+				   });
     return (
-			<input
-				 type="text"
-				 value={this.state.query}
-				 onChange={event => this.updateQuery(event.target.value)}
-			 >
-			</input>
+			<div>
+				<input
+					 type="text"
+					 value={this.state.query}
+					 onChange={event => this.updateQuery(event.target.value)}
+				 >
+				</input>
+				<div>
+				  {listMap}
+				</div>
+			</div>
     )
   }
 }
