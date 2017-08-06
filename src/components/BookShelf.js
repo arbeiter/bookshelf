037b11f982ad4c01e1f8bf5500b8cd9fdabe80e8
Siddharth Chauhan <csiddha@elemental.com>
2017-08-06 15:02:24 -0700

    Add bookshelf code and book component

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 
 class BookShelf extends Component {
-  render() {
-	const { books } = this.props;
-    const listMap = books.map((book, index) => {
-		return <li key={book.id}>
-			<ul>{book.title}</ul>
-		</li>
-	});
-	if (books!==[]){
-		return (
-			<div>
-					Hello World
-					{listMap}
-			</div>
-		)
-	 }
-	return (<div>No books to read</div>);
- }
+          render() {
+            const { books } = this.props;
+            const listMap = books.map((book, index) => {
+                        return <li key={book.id}>
+                                <ul>{book.title}</ul>
+                        </li>
+                });
+
+                if (books!==[])
+                {
+                        return (
+                                <div>
+                                                Hello World
+                                                {listMap}
+                                </div>
+                        )
+                 }
+                return (<div>No books to read</div>);
+         }
 }
 
 export default BookShelf;
